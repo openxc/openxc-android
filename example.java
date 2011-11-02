6d35afe9befa327a4b67713dccdbccbed1460c3b
Christopher Peplin <chris.peplin@rhubarbtech.com>
2011-11-02 18:38:53 -0400

    Remove unused file.

--- example.java
+++
@@ -1,9 +0,0 @@
-public class VehicleMeasurementService  extends Service {
-
-    public Measurement<TheUnit extends Unit, ID extends String>
-}
-
-public class Measurement {
-}
-
-Measurement<Unit.MperS, signals.VEHICLE_SPEED_ID> -> VehicleSpeed
