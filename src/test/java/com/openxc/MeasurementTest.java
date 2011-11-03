6a2b831c2ba24e37119c649f82baac49e8b14860
Christopher Peplin <cpeplin@ford.com>
2011-11-03 14:03:28 -0400

    Test measurement with a range.

---
+++ src/test/java/com/openxc/MeasurementTest.java
@@ -0,0 +1,45 @@
+package com.openxc;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+import com.openxc.units.Meter;
+import com.openxc.util.Range;
+import com.openxc.measurements.Measurement;
+import com.openxc.measurements.NoValueException;
+import com.openxc.measurements.NoRangeException;
+
+public class MeasurementTest {
+    Measurement<Meter> measurement;
+
+    @Before
+    public void setUp() {
+        measurement = new Measurement<Meter>(new Meter(10.0),
+                new Range<Meter>(new Meter(0.0), new Meter(101.2)));
+    }
+
+    @Test
+    public void testHasValue() {
+        assertThat(measurement.hasValue(), equalTo(true));
+    }
+
+    @Test
+    public void testGet() throws NoValueException {
+        assertThat(measurement.getValue().doubleValue(), equalTo(10.0));
+    }
+
+    @Test
+    public void testHasRange() {
+        assertTrue(measurement.hasRange());
+    }
+
+    @Test
+    public void testGetRange() throws NoRangeException {
+        assertThat(measurement.getRange().getMin().doubleValue(), equalTo(0.0));
+        assertThat(measurement.getRange().getMax().doubleValue(), equalTo(101.2));
+    }
+}
