16d79c0315dc32415290d642da5082ed89d88798
Christopher Peplin <cpeplin@ford.com>
2011-11-03 13:55:10 -0400

    Test measurements with a value but no range.

---
+++ src/test/java/com/openxc/NoRangeMeasurementTest.java
@@ -0,0 +1,42 @@
+package com.openxc;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertFalse;
+
+import com.openxc.units.Meter;
+import com.openxc.measurements.Measurement;
+import com.openxc.measurements.NoValueException;
+import com.openxc.measurements.NoRangeException;
+
+public class NoRangeMeasurementTest {
+    Measurement<Meter> measurement;
+
+    @Before
+    public void setUp() {
+        measurement = new Measurement<Meter>(new Meter(10.0));
+    }
+
+    @Test
+    public void testHasValue() {
+        assertThat(measurement.hasValue(), equalTo(true));
+    }
+
+    @Test
+    public void testHasNoRange() {
+        assertFalse(measurement.hasRange());
+    }
+
+    @Test(expected=NoRangeException.class)
+    public void testEmptyRange() throws NoRangeException {
+        measurement.getRange();
+    }
+
+    @Test
+    public void testGet() throws NoValueException {
+        assertThat(measurement.getValue().doubleValue(), equalTo(10.0));
+    }
+}
