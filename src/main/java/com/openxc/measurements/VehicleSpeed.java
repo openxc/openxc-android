89fc2f4bafc5f815baa8929a8048cd3a35e75ad7
Christopher Peplin <cpeplin@ford.com>
2011-11-03 14:32:05 -0400

    Test VehicleSpeed.

--- src/main/java/com/openxc/measurements/VehicleSpeed.java
+++ src/main/java/com/openxc/measurements/VehicleSpeed.java
@@ -1,6 +1,16 @@
 package com.openxc.measurements;
 
 import com.openxc.units.MetersPerSecond;
+import com.openxc.util.Range;
 
 public class VehicleSpeed extends Measurement<MetersPerSecond> {
+    // TODO this should change based on the vehicle platform - needs to be read
+    // from the CAN translator (which has this info stored statically)
+    private final static Range<MetersPerSecond> RANGE =
+        new Range<MetersPerSecond>(new MetersPerSecond(0.0),
+                new MetersPerSecond(60.0));
+
+    public VehicleSpeed(MetersPerSecond value) {
+        super(value, RANGE);
+    }
 }
---
+++ src/test/java/com/openxc/measurements/VehicleSpeedTest.java
@@ -0,0 +1,30 @@
+package com.openxc.measurements;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+import com.openxc.units.MetersPerSecond;
+import com.openxc.measurements.NoValueException;
+
+public class VehicleSpeedTest {
+    VehicleSpeed measurement;
+
+    @Before
+    public void setUp() {
+        measurement = new VehicleSpeed(new MetersPerSecond(1.0));
+    }
+
+    @Test
+    public void testGet() throws NoValueException {
+        assertThat(measurement.getValue().doubleValue(), equalTo(1.0));
+    }
+
+    @Test
+    public void testHasRange() {
+        assertTrue(measurement.hasRange());
+    }
+}
