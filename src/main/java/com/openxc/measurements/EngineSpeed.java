8bf7b9a9bc4f0d9b0f8b6ac3c88a64f853573929
Christopher Peplin <chris.peplin@rhubarbtech.com>
2011-11-04 13:24:34 -0400

    Remove duplicated comment.

--- src/main/java/com/openxc/measurements/EngineSpeed.java
+++ src/main/java/com/openxc/measurements/EngineSpeed.java
@@ -3,11 +3,6 @@ package com.openxc.measurements;
 import com.openxc.units.RotationsPerMinute;
 import com.openxc.util.Range;
 
-/* This may seem like a "psuedo-typedef" class but we're using it it to enforce
- * the binding between the measurement and its unit type. This unfortunately
- * means we have to add constructors because they aren't inherited from
- * Measurement. If you know of a better way, please speak up.
- */
 public class EngineSpeed extends Measurement<RotationsPerMinute>
         implements VehicleMeasurement {
     private final static Range<RotationsPerMinute> RANGE =
--- src/main/java/com/openxc/measurements/TransmissionGearPosition.java
+++ src/main/java/com/openxc/measurements/TransmissionGearPosition.java
@@ -2,11 +2,6 @@ package com.openxc.measurements;
 
 import com.openxc.units.State;
 
-/* This may seem like a "psuedo-typedef" class but we're using it it to enforce
- * the binding between the measurement and its unit type. This unfortunately
- * means we have to add constructors because they aren't inherited from
- * Measurement. If you know of a better way, please speak up.
- */
 public class TransmissionGearPosition
         extends Measurement<State<TransmissionGearPosition.GearPosition>>
         implements VehicleMeasurement {
