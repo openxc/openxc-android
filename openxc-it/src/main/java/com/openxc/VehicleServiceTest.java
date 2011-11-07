36819c3eeaa1976e39355874d794432af4c62162
Christopher Peplin <cpeplin@ford.com>
2011-11-07 14:34:24 -0500

    Test that the VehicleService can be started and bound.

--- openxc-it/src/main/java/com/openxc/VehicleServiceTest.java
+++ openxc-it/src/main/java/com/openxc/VehicleServiceTest.java
@@ -2,11 +2,18 @@ package com.openxc;
 
 import com.openxc.VehicleService;
 
+import android.content.Intent;
+
+import android.os.IBinder;
+
 import android.test.ServiceTestCase;
 
+import android.test.suitebuilder.annotation.MediumTest;
 import android.test.suitebuilder.annotation.SmallTest;
 
 public class VehicleServiceTest extends ServiceTestCase<VehicleService> {
+    Intent startIntent;
+
     public VehicleServiceTest() {
         super(VehicleService.class);
     }
@@ -14,10 +21,21 @@ public class VehicleServiceTest extends ServiceTestCase<VehicleService> {
     @Override
     protected void setUp() throws Exception {
         super.setUp();
+        startIntent = new Intent();
+        startIntent.setClass(getContext(), VehicleService.class);
     }
 
     @SmallTest
     public void testPreconditions() {
-        assertTrue(false);
+    }
+
+    @SmallTest
+    public void testStartable() {
+        startService(startIntent);
+    }
+
+    @MediumTest
+    public void testBindable() {
+        IBinder service = bindService(startIntent);
     }
 }
