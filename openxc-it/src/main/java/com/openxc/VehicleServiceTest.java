3e36545ceaf18595123ffdf1c6c86b0c6115c74c
Christopher Peplin <cpeplin@ford.com>
2011-11-07 14:10:53 -0500

    Fix dependency issues to get instrumentation tests running.

--- openxc-it/src/main/java/com/openxc/VehicleServiceTest.java
+++ openxc-it/src/main/java/com/openxc/VehicleServiceTest.java
@@ -1,4 +1,23 @@
+package com.openxc;
+
 import com.openxc.VehicleService;
 
-public class VehicleServiceTest extends ServiceTestcase<VehicleService> {
+import android.test.ServiceTestCase;
+
+import android.test.suitebuilder.annotation.SmallTest;
+
+public class VehicleServiceTest extends ServiceTestCase<VehicleService> {
+    public VehicleServiceTest() {
+        super(VehicleService.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+
+    @SmallTest
+    public void testPreconditions() {
+        assertTrue(false);
+    }
 }
