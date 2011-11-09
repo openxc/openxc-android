9a33b39e38652049cc8140229edfad86ffe7990e
Christopher Peplin <chris.peplin@rhubarbtech.com>
2011-11-09 12:14:08 -0500

    Parse JSON messages and call handleMessage with the result.

--- openxc/src/main/java/com/openxc/remote/sources/JsonVehicleDataSource.java
+++ openxc/src/main/java/com/openxc/remote/sources/JsonVehicleDataSource.java
@@ -1,7 +1,13 @@
 package com.openxc.remote.sources;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.util.Log;
+
 public abstract class JsonVehicleDataSource
         extends AbstractVehicleDataSource {
+    private static final String TAG = "JsonVehicleDataSource";
 
     public JsonVehicleDataSource() {
         super();
@@ -12,7 +18,30 @@ public abstract class JsonVehicleDataSource
     }
 
     protected void parseJson(String json) {
-        handleMessage("foo", "bar");
-        handleMessage("foo", 42);
+        final JSONObject message;
+
+        try {
+            message = new JSONObject(json);
+        } catch(JSONException e) {
+            Log.i(TAG, "Couldn't decode JSON from: " + json);
+            return;
+        }
+
+        try {
+            handleMessage(message.getString("name"),
+                    message.getDouble("value"));
+            return;
+        } catch(JSONException e) {
+            // might be a string value
+            Log.d(TAG, "Couldn't parse a double from JSON " + message +
+                    " -- trying String next");
+        }
+        try {
+            handleMessage(message.getString("name"),
+                    message.getString("value"));
+        } catch(JSONException e) {
+            Log.w(TAG, "JSON value wasn't a double or string -- couldn't parse",
+                    e);
+        }
     }
 }
