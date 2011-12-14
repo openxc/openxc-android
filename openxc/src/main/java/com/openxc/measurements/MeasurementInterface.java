cc4ab68c412c346a62c2448746e0dae8e6496ffa
Christopher Peplin <cpeplin@ford.com>
2011-12-14 15:14:29 -0500

    Document methods in the interface, not the implementation.

--- openxc/src/main/java/com/openxc/measurements/MeasurementInterface.java
+++ openxc/src/main/java/com/openxc/measurements/MeasurementInterface.java
@@ -12,9 +12,40 @@ import com.openxc.util.Range;
  * validity first with {@link #isNone()}.
  */
 public interface MeasurementInterface {
+    /**
+     * Check the validity of this measurement.
+     *
+     * @return true if this measurement has no value.
+     */
     public boolean isNone();
+
+    /**
+     * Retreive the age of this measurement.
+     *
+     * @return the age of the data in seconds.
+     */
     public double getAge() throws NoValueException;
+
+    /**
+     * Determine if this measurement has a valid range.
+     *
+     * @return true if the measurement has a non-null range.
+     */
     public boolean hasRange();
+
+    /**
+     * Retrieve the valid range of the measurement.
+     *
+     * @return the Range of the measurement
+     * @throws NoRangeException if the measurement doesn't have a range.
+     */
     public Range<? extends Unit> getRange() throws NoRangeException;
+
+    /**
+     * Return the value of this measurement.
+     *
+     * @return The wrapped value (an instance of TheUnit)
+     * @throws NoValueException if this measurement has no value
+     */
     public Unit getValue() throws NoValueException;
 }
