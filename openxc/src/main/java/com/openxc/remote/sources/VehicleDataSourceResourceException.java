5957990535f2c058515ee1c3f3d6bbdd18da95ee
Christopher Peplin <chris.peplin@rhubarbtech.com>
2012-02-15 13:58:05 -0800

    Add generated serialVersionUID for some exception classes.

--- openxc/src/main/java/com/openxc/remote/sources/VehicleDataSourceResourceException.java
+++ openxc/src/main/java/com/openxc/remote/sources/VehicleDataSourceResourceException.java
@@ -2,6 +2,11 @@ package com.openxc.remote.sources;
 
 public class VehicleDataSourceResourceException
         extends VehicleDataSourceException {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8969542459264680692L;
+
 	public VehicleDataSourceResourceException(String message, Throwable cause) {
         super(message, cause);
     }
