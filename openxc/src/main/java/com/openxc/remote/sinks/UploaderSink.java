420765955f7f5b5a56320059af140db3d317db8b
Christopher Peplin <chris.peplin@rhubarbtech.com>
2012-03-28 23:55:57 -0400

    Upload trace records to a remote server.

--- openxc/src/main/java/com/openxc/remote/sinks/UploaderSink.java
+++ openxc/src/main/java/com/openxc/remote/sinks/UploaderSink.java
@@ -4,6 +4,7 @@ import java.io.BufferedWriter;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 
 import java.util.Date;
 import java.util.ArrayList;
@@ -15,6 +16,18 @@ import java.text.SimpleDateFormat;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.json.JSONArray;
+
+import org.apache.http.client.HttpClient;
+import org.apache.http.entity.ByteArrayEntity;
+
+import org.apache.http.client.methods.HttpPost;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.message.BasicHeader;
+
+import org.apache.http.impl.client.DefaultHttpClient;
 
 import com.openxc.remote.RawMeasurement;
 
@@ -23,23 +36,32 @@ import android.util.Log;
 
 public class UploaderSink implements VehicleDataSinkInterface {
     private final static String TAG = "UploaderSink";
-    private final static int UPLOAD_BATCH_SIZE = 100;
+    private final static String UPLOAD_URL =
+            "http://fiesta.eecs.umich.edu:5000/records";
+    private final static int UPLOAD_BATCH_SIZE = 200;
+    private final static int MAXIMUM_QUEUED_RECORDS = 2000;
+    private static DecimalFormat sTimestampFormatter =
+            new DecimalFormat("##########.000000");
 
-    private BlockingQueue<String> mRecordQueue;
+    private BlockingQueue<JSONObject> mRecordQueue;
+    private HttpClient mClient;
 
     public UploaderSink(Context context) {
-        mRecordQueue = new LinkedBlockingQueue<String>();
+        mRecordQueue = new LinkedBlockingQueue<JSONObject>(
+                MAXIMUM_QUEUED_RECORDS);
+        mClient = new DefaultHttpClient();
     }
 
     public void stop() { }
 
     public void receive(String measurementId, Object value, Object event) {
         double timestamp = System.currentTimeMillis() / 1000.0;
+        String timestampString = sTimestampFormatter.format(timestamp);
 
         JSONObject object = new JSONObject();
         try {
             object.put("name", measurementId);
-            object.put("timestamp", timestamp);
+            object.put("timestamp", timestampString);
             object.put("value", value);
             if(event != null) {
                 object.put("event", event);
@@ -48,7 +70,7 @@ public class UploaderSink implements VehicleDataSinkInterface {
             Log.w(TAG, "Unable to create JSON for trace file", e);
             return;
         }
-        mRecordQueue.add(object.toString());
+        mRecordQueue.offer(object);
         if(mRecordQueue.size() >= UPLOAD_BATCH_SIZE) {
             (new UploaderThread()).start();
         }
@@ -58,9 +80,46 @@ public class UploaderSink implements VehicleDataSinkInterface {
         private boolean mRunning = true;
 
         public void run() {
-            ArrayList<String> records = new ArrayList();
+            ArrayList<JSONObject> records = new ArrayList();
             mRecordQueue.drainTo(records, UPLOAD_BATCH_SIZE);
-            Log.d(TAG, "Would have uploaded " + records.size() + " items");
+
+            JSONArray recordArray = new JSONArray();
+            for(JSONObject record : records) {
+                recordArray.put(record);
+            }
+
+            JSONObject data = new JSONObject();
+            try {
+                data.put("records", recordArray);
+            } catch(JSONException e) {
+                Log.w(TAG, "Unable to create JSON for uploading records", e);
+                return;
+            }
+
+            final HttpClient client = new DefaultHttpClient();
+            HttpPost request = new HttpPost(UPLOAD_URL);
+            try {
+                ByteArrayEntity entity = new ByteArrayEntity(
+                        data.toString().getBytes("UTF8"));
+                entity.setContentEncoding(
+                        new BasicHeader("Content-Type", "application/json"));
+                request.setEntity(entity);
+            } catch(UnsupportedEncodingException e) {
+                Log.w(TAG, "Couldn't encode records for uploading", e);
+                return;
+            }
+
+            try {
+                HttpResponse response = client.execute(request);
+                final int statusCode = response.getStatusLine().getStatusCode();
+                if(statusCode == HttpStatus.SC_CREATED) {
+                    Log.d(TAG, "Uploaded " + records.size() + " records");
+                } else {
+                    Log.w(TAG, "Got unxpected status code: " + statusCode);
+                }
+            } catch(IOException e) {
+                Log.w(TAG, "Problem uploading the record", e);
+            }
         }
     }
 }
