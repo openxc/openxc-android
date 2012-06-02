3473ac05f9197a5d32f41f36ea13541b990ffe85
Christopher Peplin <chris.peplin@rhubarbtech.com>
2012-06-01 20:41:41 -0400

    Remove powermock to lower complexity of dependencies for testing.

--- openxc-it/src/main/java/com/openxc/sinks/FileRecorderSinkTest.java
+++ openxc-it/src/main/java/com/openxc/sinks/FileRecorderSinkTest.java
@@ -21,20 +21,12 @@ import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-import org.mockito.stubbing.Answer;
-import org.powermock.api.mockito.PowerMockito;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
-
 import com.openxc.util.FileOpener;
 
-import android.util.Log;
+import android.test.AndroidTestCase;
+import android.test.suitebuilder.annotation.SmallTest;
 
-@RunWith(PowerMockRunner.class)
-@PrepareForTest({
-    Log.class
-})
-public class FileRecorderSinkTest {
+public class FileRecorderSinkTest extends AndroidTestCase {
     FileRecorderSink sink;
     FileOpener opener;
     StringWriter outputString;
@@ -45,13 +37,12 @@ public class FileRecorderSinkTest {
 
     @Before
     public void setUp() throws IOException {
-        mockLogger();
         outputString = new StringWriter();
         opener = new MockFileOpener();
         sink = new FileRecorderSink(opener);
     }
 
-    @Test
+    @SmallTest
     public void testReceiveValueOnly() {
         assertThat(outputString.toString(), not(containsString(measurementId)));
         sink.receive(measurementId, value);
@@ -60,7 +51,7 @@ public class FileRecorderSinkTest {
         assertThat(outputString.toString(), containsString(value));
     }
 
-    @Test
+    @SmallTest
     public void testReceiveEvented() {
         sink.receive(measurementId, value, event);
         sink.flush();
@@ -69,7 +60,7 @@ public class FileRecorderSinkTest {
         assertThat(outputString.toString(), containsString(event));
     }
 
-    @Test
+    @SmallTest
     public void testOutputFormat() throws JSONException {
         sink.receive(measurementId, value);
         sink.flush();
@@ -84,28 +75,11 @@ public class FileRecorderSinkTest {
         assertThat(message.getString("value"), equalTo(value));
     }
 
-    @Test
+    @SmallTest
     public void testStop() {
         assertTrue(sink.isRunning());
         sink.stop();
         assertFalse(sink.isRunning());
-    }
-
-    private void mockLogger() {
-        PowerMockito.mockStatic(Log.class);
-        when(Log.d(anyString(), anyString())).thenAnswer(new Answer<Integer>()
-        {
-
-            @Override
-            public Integer answer(final InvocationOnMock invocation) throws Throwable
-            {
-                final String tag = (String) invocation.getArguments()[0];
-                final String msg = (String) invocation.getArguments()[1];
-                System.out.println("[" + tag + "] " + msg);
-                return 0;
-            }
-        });
-
     }
 
     private class MockFileOpener implements FileOpener {
