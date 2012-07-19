8d62a2d8405cdb8bcfccc255f863360913a5ba84
Christopher Peplin <chris.peplin@rhubarbtech.com>
2012-07-19 14:08:11 -0400

    Add a script to automate updating the javadocs in the gh-pages branch.

---
+++ scripts/updatedocs.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+CURRENT_BRANCH=`git symbolic-ref HEAD 2>/dev/null | awk -F/ {'print $NF'}`
+git checkout master
+mvn clean package javadoc:javadoc -pl openxc
+TEMP_PATH=/tmp/openxc-apidocs
+rm -rf $TEMP_PATH
+cp -R openxc/target/apidocs $TEMP_PATH
+git checkout gh-pages
+rm -rf *.html references assets reference resources
+cp -R $TEMP_PATH/* .
+git add -A
+git commit -m "Update Javadocs."
+git push
+git checkout $CURRENT_BRANCH
