eda1b338e005cc1e96914a00440f58a8dcaa6299
Christopher Peplin <chris.peplin@rhubarbtech.com>
2012-12-21 17:45:53 -0500

    Fix typo in javadocs link.

--- openxc/src/com/openxc/package-info.java
+++ openxc/src/com/openxc/package-info.java
@@ -1,5 +1,5 @@
 /**
  * This is the top-level package of the OpenXC library. It contains the
- * primary entry point for applications, the {@link com.openxc.VechicleManager}.
+ * primary entry point for applications, the {@link com.openxc.VehicleManager}.
  */
 package com.openxc;
