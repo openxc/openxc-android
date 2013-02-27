94c2a97bf926e01ae68730ffdb92a9d1439b3dcc
Christopher Peplin <chris.peplin@rhubarbtech.com>
2013-02-27 17:47:20 -0500

    Maintain scroll position in data source/sink list in Enabler.
    Instead of blowing away the adapter each time we update, keep the same adapter
    and just update the list.
    
    Thanks to
    http://vikinghammer.com/2011/06/17/android-listview-maintain-your-scroll-position-when-you-refresh/

--- enabler/src/com/openxc/enabler/PipelineStatusUpdateTask.java
+++ enabler/src/com/openxc/enabler/PipelineStatusUpdateTask.java
@@ -13,6 +13,8 @@ public class PipelineStatusUpdateTask extends TimerTask {
     private Activity mActivity;
     private ListView mSourceListView;
     private ListView mSinkListView;
+    private ArrayAdapter<Object> mSourceListAdapter;
+    private ArrayAdapter<Object> mSinkListAdapter;
 
     public PipelineStatusUpdateTask(VehicleManager vehicleService,
             Activity activity, ListView sourceListView, ListView sinkListView) {
@@ -20,17 +22,28 @@ public class PipelineStatusUpdateTask extends TimerTask {
         mActivity = activity;
         mSourceListView = sourceListView;
         mSinkListView = sinkListView;
+
+        mSourceListAdapter = new ArrayAdapter<Object>(mActivity,
+                android.R.layout.simple_list_item_1);
+        mSourceListView.setAdapter(mSourceListAdapter);
+
+        mSinkListAdapter = new ArrayAdapter<Object>(mActivity,
+                android.R.layout.simple_list_item_1);
+        mSinkListView.setAdapter(mSinkListAdapter);
     }
 
     public void run() {
         mActivity.runOnUiThread(new Runnable() {
             public void run() {
-                mSourceListView.setAdapter(new ArrayAdapter<Object>(mActivity,
-                            android.R.layout.simple_list_item_1,
-                            mVehicleManager.getSourceSummaries().toArray()));
-                mSinkListView.setAdapter(new ArrayAdapter<Object>(mActivity,
-                            android.R.layout.simple_list_item_1,
-                            mVehicleManager.getSinkSummaries().toArray()));
+                mSourceListAdapter.clear();
+                mSourceListAdapter.addAll(
+                    mVehicleManager.getSourceSummaries().toArray());
+                mSourceListAdapter.notifyDataSetChanged();
+
+                mSinkListAdapter.clear();
+                mSinkListAdapter.addAll(
+                    mVehicleManager.getSinkSummaries().toArray());
+                mSinkListAdapter.notifyDataSetChanged();
             }
         });
     }
