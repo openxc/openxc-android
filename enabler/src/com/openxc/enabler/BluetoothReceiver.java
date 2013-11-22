f486300a5ee7b2d130b673ceceeee7360b569f0f
Christopher Peplin <chris.peplin@rhubarbtech.com>
2013-11-22 16:13:37 -0500

    Store VI Bluetooth device name prefix in a central place.

--- enabler/src/com/openxc/enabler/BluetoothReceiver.java
+++ enabler/src/com/openxc/enabler/BluetoothReceiver.java
@@ -1,42 +1,41 @@
 package com.openxc.enabler;
 
-import com.openxc.VehicleManager;
-import com.openxc.enabler.preferences.PreferenceManagerService;
-
 import android.bluetooth.BluetoothDevice;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 
-/**
- * Receive the BOOT_COMPLETED signal and start the VehicleManager.
- *
- * The reason to do this in a central location is to centralize USB permissions
- * management.
- */
+import com.openxc.VehicleManager;
+import com.openxc.enabler.preferences.PreferenceManagerService;
+import com.openxc.interfaces.bluetooth.BluetoothVehicleInterface;
+
 public class BluetoothReceiver extends BroadcastReceiver {
     private final static String TAG = BluetoothReceiver.class.getSimpleName();
 
-    // TODO what about when the device is already started? need an app to hit?
-    // or do we rely on it being started by the bind call? might get duplicate
-    // USB permission requests that way, but maybe it's OK.
     @Override
     public void onReceive(Context context, Intent intent) {
-    	Log.d(TAG, "Recieved intent Event: " + intent.getAction());
-    	
-    	// If "OpenXC-VI-*" is connected via Bluetooth, start service
-    	if(intent.getAction().compareTo(BluetoothDevice.ACTION_ACL_CONNECTED) == 0){
-    		BluetoothDevice bluetoothDevice = (BluetoothDevice)intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-    		
-    		if(bluetoothDevice != null 
-    				&& bluetoothDevice.getName() != null
-    				&& bluetoothDevice.getName().contains("OpenXC-VI-")){    	
-    			
-    	        Log.i(TAG, "Starting vehicle service on bluetooth connection to 'OpenXC-VI-*'.");
-    	        context.startService(new Intent(context, VehicleManager.class));
-    	        context.startService(new Intent(context, PreferenceManagerService.class));
-    		}
-    	}
+        Log.d(TAG, "Recieved intent Event: " + intent.getAction());
+
+        // If a Bluetooth device with the OpenXC device name prefix is
+        // connected, start the service if it's not already started
+        if(intent.getAction().compareTo(
+                    BluetoothDevice.ACTION_ACL_CONNECTED) == 0){
+            BluetoothDevice bluetoothDevice = (BluetoothDevice)
+                intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+
+            if(bluetoothDevice != null
+                    && bluetoothDevice.getName() != null
+                    && bluetoothDevice.getName().contains(
+                        BluetoothVehicleInterface.DEVICE_NAME_PREFIX)){
+
+                Log.i(TAG, "Starting vehicle service on bluetooth connection " +
+                        "to " + BluetoothVehicleInterface.DEVICE_NAME_PREFIX +
+                        "*.");
+                context.startService(new Intent(context, VehicleManager.class));
+                context.startService(new Intent(context,
+                            PreferenceManagerService.class));
+            }
+        }
     }
 }
