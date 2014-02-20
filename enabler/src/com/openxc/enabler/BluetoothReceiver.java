0a3d69f0cb3238462b95ccd61fb23617b15d15ff
Christopher Peplin <chris.peplin@rhubarbtech.com>
2014-02-20 17:00:37 -0500

    Log when VIs connect and disconnect at the lowest level.

--- enabler/src/com/openxc/enabler/BluetoothReceiver.java
+++ enabler/src/com/openxc/enabler/BluetoothReceiver.java
@@ -13,28 +13,25 @@ import com.openxc.interfaces.bluetooth.BluetoothVehicleInterface;
 public class BluetoothReceiver extends BroadcastReceiver {
     private final static String TAG = BluetoothReceiver.class.getSimpleName();
 
+    private final boolean isVehicleInterface(BluetoothDevice device) {
+        return device != null && device.getName() != null &&
+                device.getName().contains(
+                    BluetoothVehicleInterface.DEVICE_NAME_PREFIX);
+    }
+
     @Override
     public void onReceive(Context context, Intent intent) {
-        Log.d(TAG, "Recieved intent, event: " + intent.getAction());
-
-        // If a Bluetooth device with the OpenXC device name prefix is
-        // connected, start the service if it's not already started
-        if(intent.getAction().compareTo(
-                    BluetoothDevice.ACTION_ACL_CONNECTED) == 0){
-            BluetoothDevice bluetoothDevice = (BluetoothDevice)
-                intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-
-            if(bluetoothDevice != null
-                    && bluetoothDevice.getName() != null
-                    && bluetoothDevice.getName().contains(
-                        BluetoothVehicleInterface.DEVICE_NAME_PREFIX)){
-
-                Log.i(TAG, "Starting vehicle service on bluetooth connection " +
-                        "to " + BluetoothVehicleInterface.DEVICE_NAME_PREFIX +
-                        "*.");
+        BluetoothDevice bluetoothDevice = (BluetoothDevice)
+            intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+        if(isVehicleInterface(bluetoothDevice)) {
+            if(intent.getAction().compareTo(
+                        BluetoothDevice.ACTION_ACL_CONNECTED) == 0){
+                Log.d(TAG, "A Bluetooth OpenXC VI connected: " + bluetoothDevice.getName());
                 context.startService(new Intent(context, VehicleManager.class));
                 context.startService(new Intent(context,
                             PreferenceManagerService.class));
+            } else {
+                Log.d(TAG, "A Bluetooth OpenXC VI disconnected: " + bluetoothDevice.getName());
             }
         }
     }
