a52453de64da3ae05d493ea84f0a9c149d8abf4e
Charles Reitz <cgreitz@hotmail.com>
2014-05-28 13:30:35 -0400

    negative response codes

--- openxc/src/com/openxc/messages/DiagnosticResponse.java
+++ openxc/src/com/openxc/messages/DiagnosticResponse.java
@@ -10,7 +10,29 @@ public class DiagnosticResponse extends VehicleMessage {
     private float mValue;
     private NegativeResponseCode mNegativeResponseCode;
 
-    public enum NegativeResponseCode {
-        // TODO
+    public static enum NegativeResponseCode {
+        subFunctionNotSupported(0x12),
+        incorrectMessageLengthOrInvalidFormat(0x13),
+        busyRepeatRequest(0x21),
+        conditionsNotCorrect(0x22),
+        requestSequenceError(0x24),
+        requestOutOfRange(0x31),
+        securityAccessDenied(0x33),
+        invalidKey(0x35),
+        exceedNumberOfAttempts(0x36),
+        requiredTimeDelayNotExpired(0x37),
+        uploadDownloadNotAccepted(0x70),
+        wrongBlockSequenceCounter(0x73),
+        subFunctionNotSupportedInActiveSession(0x7E),
+        serviceNotSupportedInActiveSession(0x7F);
+
+        private int value;
+        NegativeResponseCode(int value) {
+        	this.value = value;        	
+        }
+        
+        public int value() {
+        	return this.value;
+        }
     };
 }
