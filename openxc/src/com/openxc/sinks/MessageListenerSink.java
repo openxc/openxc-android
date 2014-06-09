3f5e6bc1f0209e4dd90e53a232804ffc484fef3d
Christopher Peplin <chris.peplin@rhubarbtech.com>
2014-06-08 20:15:02 -0400

    Merge in DiagnosticResponse listener notification.

--- openxc/src/com/openxc/sinks/MessageListenerSink.java
+++ openxc/src/com/openxc/sinks/MessageListenerSink.java
@@ -1,5 +1,8 @@
 package com.openxc.sinks;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import android.util.Log;
 
 import com.google.common.base.Objects;
@@ -10,9 +13,11 @@ import com.openxc.NoValueException;
 import com.openxc.measurements.BaseMeasurement;
 import com.openxc.measurements.Measurement;
 import com.openxc.measurements.UnrecognizedMeasurementTypeException;
+import com.openxc.messages.CommandResponse;
+import com.openxc.messages.DiagnosticRequest;
+import com.openxc.messages.DiagnosticResponse;
 import com.openxc.messages.NamedVehicleMessage;
 import com.openxc.messages.VehicleMessage;
-import com.openxc.messages.SimpleVehicleMessage;
 
 /**
  * A data sink that sends new measurements of specific types to listeners.
@@ -23,18 +28,23 @@ import com.openxc.messages.SimpleVehicleMessage;
 public class MessageListenerSink extends AbstractQueuedCallbackSink {
     private final static String TAG = "MessageListenerSink";
 
-    private Multimap<Class<? extends Measurement>,
-            Measurement.Listener> mListeners = HashMultimap.create();
+    private Multimap<Class<? extends Measurement>, Measurement.Listener>
+            mMeasurementListeners = HashMultimap.create();
+    private Multimap<String, DiagnosticResponse.Listener>
+            mDiagnosticListeners = HashMultimap.create();
+    private Map<String, DiagnosticRequest> mRequestMap = new HashMap<>();
 
     public MessageListenerSink() {
-        mListeners = HashMultimap.create();
-        mListeners = Multimaps.synchronizedMultimap(mListeners);
+        mMeasurementListeners = Multimaps.synchronizedMultimap(
+                mMeasurementListeners);
+        mDiagnosticListeners = Multimaps.synchronizedMultimap(
+                mDiagnosticListeners);
     }
 
     public void register(Class<? extends Measurement> measurementType,
             Measurement.Listener listener)
             throws UnrecognizedMeasurementTypeException {
-        mListeners.put(measurementType, listener);
+        mMeasurementListeners.put(measurementType, listener);
 
         String name = BaseMeasurement.getIdForClass(measurementType);
         if(containsNamedMessage(name)) {
@@ -47,33 +57,58 @@ public class MessageListenerSink extends AbstractQueuedCallbackSink {
         }
     }
 
+    public void registerUntilDone(DiagnosticRequest request,
+            DiagnosticResponse.Listener listener) {
+        String diagnosticIdentifier = request.getDiagnosticIdentifier();
+        // Sending a request with the same diagnosticIdentifier as a previous
+        // one cancels the last one, so just overwrite it
+        mRequestMap.put(diagnosticIdentifier, request);
+        mDiagnosticListeners.put(diagnosticIdentifier, listener);
+    }
+
     public void unregister(Class<? extends Measurement> measurementType,
             Measurement.Listener listener) {
-        mListeners.remove(measurementType, listener);
+        mMeasurementListeners.remove(measurementType, listener);
     }
 
     @Override
     public String toString() {
         return Objects.toStringHelper(this)
-            .add("numListeners", mListeners.size())
+            .add("numListeners", mMeasurementListeners.size())
             .toString();
     }
 
     protected void propagateMessage(VehicleMessage message) {
         if(message instanceof NamedVehicleMessage) {
             propagateMeasurement((NamedVehicleMessage) message);
+        } else if(message instanceof DiagnosticResponse) {
+            propagateDiagnosticResponse((DiagnosticResponse) message);
         } else {
             // TODO propagate generic VehicleMessage - need a new listener
             // callback
         }
     }
 
-    protected void propagateMeasurement(NamedVehicleMessage message) {
+    private void propagateDiagnosticResponse(DiagnosticResponse response) {
+        String diagnosticIdentifier = response.getDiagnosticIdentifier();
+        DiagnosticRequest request = mRequestMap.get(diagnosticIdentifier);
+        for(DiagnosticResponse.Listener listener : mDiagnosticListeners.get(diagnosticIdentifier)) {
+            listener.receive(request, response);
+        }
+        //if the frequency of the most recent request with this id is 0, it's done once the response has been received,
+        //so no need to store them anymore
+        if (request.getFrequency() == 0) {
+            mRequestMap.remove(diagnosticIdentifier);
+            mDiagnosticListeners.removeAll(diagnosticIdentifier);
+        }
+    }
+
+    private void propagateMeasurement(NamedVehicleMessage message) {
         try {
             Measurement measurement =
                 BaseMeasurement.getMeasurementFromMessage(message);
             for(Measurement.Listener listener :
-                    mListeners.get(measurement.getClass())) {
+                    mMeasurementListeners.get(measurement.getClass())) {
                 listener.receive(measurement);
             }
         } catch(UnrecognizedMeasurementTypeException e) {
