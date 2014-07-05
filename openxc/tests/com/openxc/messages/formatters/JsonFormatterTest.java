809f3754dafb79315ac6df33d672c18c6f3e41f6
Christopher Peplin <chris.peplin@rhubarbtech.com>
2014-07-05 13:09:40 -0400

    Abstract tests that can be shared for all formatters.

--- openxc/tests/com/openxc/messages/formatters/JsonFormatterTest.java
+++ openxc/tests/com/openxc/messages/formatters/JsonFormatterTest.java
@@ -6,24 +6,15 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-import java.util.HashMap;
-
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 
-import com.openxc.messages.CanMessage;
-import com.openxc.messages.Command;
-import com.openxc.messages.CommandResponse;
-import com.openxc.messages.DiagnosticRequest;
-import com.openxc.messages.DiagnosticResponse;
-import com.openxc.messages.NamedVehicleMessage;
-import com.openxc.messages.EventedSimpleVehicleMessage;
 import com.openxc.messages.SimpleVehicleMessage;
-import com.openxc.messages.UnrecognizedMessageTypeException;
 import com.openxc.messages.VehicleMessage;
+import com.openxc.messages.UnrecognizedMessageTypeException;
 
 @Config(emulateSdk = 18, manifest = Config.NONE)
 @RunWith(RobolectricTestRunner.class)
@@ -32,7 +23,7 @@ public class JsonFormatterTest {
     String messageName = "foo";
     Double value = Double.valueOf(42);
 
-    private void serializeDeserializeAndCheckEqual(
+    protected void serializeDeserializeAndCheckEqual(
             VehicleMessage originalMessage) {
         String serialized = JsonFormatter.serialize(originalMessage);
         assertFalse(serialized.isEmpty());
@@ -43,104 +34,6 @@ public class JsonFormatterTest {
         } catch(UnrecognizedMessageTypeException e) {
             Assert.fail(e.toString());
         }
-    }
-
-    @Test
-    public void serializeDiagnosticResponse() {
-        serializeDeserializeAndCheckEqual(new DiagnosticResponse(
-                    1, 2, 3, 4,
-                    new byte[]{1,2,3,4}));
-    }
-
-    @Test
-    public void serializeDiagnosticResponseWithValue() {
-        serializeDeserializeAndCheckEqual(new DiagnosticResponse(
-                    1, 2, 3, 4,
-                    new byte[]{1,2,3,4}, null, 42.0));
-    }
-
-    @Test
-    public void serializeDiagnosticRequest() {
-        DiagnosticRequest request = new DiagnosticRequest(1, 2, 3, 4);
-        serializeDeserializeAndCheckEqual(request);
-    }
-
-    @Test
-    public void serializeDiagnosticRequestWithOptional() {
-        DiagnosticRequest request = new DiagnosticRequest(1, 2, 3, 4);
-        request.setPayload(new byte[]{1,2,3,4});
-        request.setMultipleResponses(false);
-        request.setFrequency(2.0);
-        request.setName("foo");
-        serializeDeserializeAndCheckEqual(request);
-    }
-
-    @Test
-    public void serializeCommandResponse() {
-        serializeDeserializeAndCheckEqual(new CommandResponse("foo"));
-    }
-
-    @Test
-    public void serializeCommandResponseWithMessage() {
-        serializeDeserializeAndCheckEqual(new CommandResponse("foo", "bar"));
-    }
-
-    @Test
-    public void serializeCommand() {
-        serializeDeserializeAndCheckEqual(new Command("foo"));
-    }
-
-    @Test
-    public void serializeCanMessage() {
-        serializeDeserializeAndCheckEqual(new CanMessage(1, 2, new byte[]{1,2,3,4}));
-    }
-
-    @Test
-    public void serializeSimpleMessage() {
-        serializeDeserializeAndCheckEqual(new SimpleVehicleMessage("foo", "bar"));
-    }
-
-    @Test
-    public void serializeEventedSimpleMessage() {
-        serializeDeserializeAndCheckEqual(new EventedSimpleVehicleMessage(
-                    "foo", "bar", "baz"));
-    }
-
-    @Test
-    public void serializeNamedMessageWithExtras() {
-        HashMap<String, Object> extras = new HashMap<>();
-        extras.put("foo", "bar");
-        extras.put("baz", 42.0);
-        VehicleMessage message = new NamedVehicleMessage("foo");
-        message.setExtras(extras);
-        serializeDeserializeAndCheckEqual(message);
-    }
-
-    @Test
-    public void serializeNamedMessage() {
-        serializeDeserializeAndCheckEqual(new NamedVehicleMessage("foo"));
-    }
-
-    @Test
-    public void serializeWithExtras() {
-        HashMap<String, Object> extras = new HashMap<>();
-        extras.put("foo", "bar");
-        extras.put("baz", 42.0);
-        VehicleMessage message = new VehicleMessage(extras);
-        serializeDeserializeAndCheckEqual(message);
-    }
-
-    @Test
-    public void serializeEmptyVehicleMessage() {
-        serializeDeserializeAndCheckEqual(new VehicleMessage());
-    }
-
-    @Test
-    public void testSerializeWithoutTimestamp() {
-        VehicleMessage message = new SimpleVehicleMessage(messageName, value);
-        message.untimestamp();
-        String serialized = new String(formatter.serialize(message));
-        assertFalse(serialized.contains("timestamp"));
     }
 
     @Test
@@ -170,5 +63,13 @@ public class JsonFormatterTest {
                     new SimpleVehicleMessage(
                         Long.valueOf(1332432977835L), messageName, value)));
         assertTrue(serialized.contains("1.332432977835E9"));
+    }
+
+    @Test
+    public void testSerializeWithoutTimestamp() {
+        VehicleMessage message = new SimpleVehicleMessage(messageName, value);
+        message.untimestamp();
+        String serialized = new String(formatter.serialize(message));
+        assertFalse(serialized.contains("timestamp"));
     }
 }
