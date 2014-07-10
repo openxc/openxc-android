0824132c4d50749ddfeb67a3646d14684cd30219
Charles Reitz <cgreitz@hotmail.com>
2014-07-10 12:53:20 -0400

    Make command keys match command responses. Keys for commands and command responses only match if they have the same pair, and the key of a CommandResponse has in its pair a key  of 'command_response' instead of 'command' before this fix.

--- openxc/src/com/openxc/messages/CommandResponse.java
+++ openxc/src/com/openxc/messages/CommandResponse.java
@@ -74,7 +74,7 @@ public class CommandResponse extends KeyedMessage {
     @Override
     public MessageKey getKey() {
         HashMap<String, Object> key = new HashMap<>();
-        key.put(COMMAND_RESPONSE_KEY, getCommand());
+        key.put(Command.COMMAND_KEY, getCommand());
         return new MessageKey(key);
     }
 
