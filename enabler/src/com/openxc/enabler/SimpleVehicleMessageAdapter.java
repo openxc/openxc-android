32dec9d4a2f68c7e8746f34f5923ca68ccd78c9e
Christopher Peplin <chris.peplin@rhubarbtech.com>
2014-08-19 00:01:34 -0400

    Display measurements with proper units in dashboard if known.

--- enabler/src/com/openxc/enabler/SimpleVehicleMessageAdapter.java
+++ enabler/src/com/openxc/enabler/SimpleVehicleMessageAdapter.java
@@ -1,11 +1,16 @@
 package com.openxc.enabler;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.openxc.NoValueException;
+import com.openxc.measurements.BaseMeasurement;
+import com.openxc.measurements.Measurement;
+import com.openxc.measurements.UnrecognizedMeasurementTypeException;
 import com.openxc.messages.SimpleVehicleMessage;
 
 public class SimpleVehicleMessageAdapter extends KeyedMessageAdapter {
@@ -29,12 +34,21 @@ public class SimpleVehicleMessageAdapter extends KeyedMessageAdapter {
         }
 
         SimpleVehicleMessage message = getItem(position);
-
         TextView nameView = (TextView) convertView.findViewById(R.id.name);
         nameView.setText("" + message.getName());
 
         TextView valueView = (TextView) convertView.findViewById(R.id.value);
-        valueView.setText("" + message.getValue());
+        try {
+            // TODO most measurements are missing from the ID cache because we
+            // haven't set up an explicit listener before - use a class loader
+            // to cache these on the fly
+            Measurement measurement =
+                BaseMeasurement.getMeasurementFromMessage(message);
+            valueView.setText("" + measurement.toString());
+        } catch(UnrecognizedMeasurementTypeException e) {
+            valueView.setText("" + message.getValue());
+        } catch(NoValueException e) {
+        }
 
         return convertView;
     }
