74f7c4359e569df70a88f10b67eee27a6ae6bd01
Christopher Peplin <chris.peplin@rhubarbtech.com>
2014-09-20 18:14:07 -0400

    Remove unused private variable.

--- openxc/src/com/openxc/interfaces/usb/UsbVehicleInterface.java
+++ openxc/src/com/openxc/interfaces/usb/UsbVehicleInterface.java
@@ -49,7 +49,6 @@ import com.openxc.sources.SourceCallback;
 public class UsbVehicleInterface extends BytestreamDataSource
         implements VehicleInterface {
     private static final String TAG = "UsbVehicleInterface";
-    private static final int VERSION_CONTROL_COMMAND = 0x80;
     private static final int CONTROL_COMMAND_TIMEOUT_S = 1;
     private static final int ENDPOINT_COUNT = 2;
 
