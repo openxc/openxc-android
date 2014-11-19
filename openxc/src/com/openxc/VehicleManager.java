346e0dff27c72855ba83000e788ecb215e08912f
Christopher Peplin <chris.peplin@rhubarbtech.com>
2014-11-18 21:08:44 -0500

    Use an explicit intent to start VehicleService.
    All along, just had to get the names matched up - I was trying to
    start VehicleServiceInterface instead of just VehicleService.
    
    This should fix this illegal argument exception crash: https://bugsnag.com/openxc/openxc-android-app/errors/546a6e1af32e078572623111

--- openxc/src/com/openxc/VehicleManager.java
+++ openxc/src/com/openxc/VehicleManager.java
@@ -823,10 +823,7 @@ public class VehicleManager extends Service implements DataPipeline.Operator {
 
     private void bindRemote() {
         Log.i(TAG, "Binding to VehicleService");
-        // TODO Android warns us that this implicit intent is unsafe, but if we
-        // try creating an explicit intent (with out context), the server is
-        // never bound. What gives?
-        Intent intent = new Intent(VehicleServiceInterface.class.getName());
+        Intent intent = new Intent(this, VehicleService.class);
         try {
             bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
             mIsBound = true;
