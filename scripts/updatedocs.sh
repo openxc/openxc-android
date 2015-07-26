b72dc6f27be2234f76c2765c7898fe237b035d25
Christopher Peplin <chris.peplin@rhubarbtech.com>
2015-07-26 18:16:06 -0400

    Update script for updating web docs to use gradle.

--- scripts/updatedocs.sh
+++ scripts/updatedocs.sh
@@ -3,11 +3,9 @@
 set -ex
 
 CURRENT_BRANCH=`git symbolic-ref HEAD 2>/dev/null | awk -F/ {'print $NF'}`
-git checkout master
-mvn clean package javadoc:javadoc -pl openxc
 TEMP_PATH=/tmp/openxc-apidocs
 rm -rf $TEMP_PATH
-cp -R openxc/target/apidocs $TEMP_PATH
+cp -R library/build/docs/javadoc/ $TEMP_PATH
 git checkout gh-pages
 git pull
 rm -rf *.html references assets reference resources
