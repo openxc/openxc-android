3cdee9cc764cf480fa51bcdc065add84b039b6fa
vkanishk <vkanishk@ford.com>
2017-04-03 16:10:13 +0530

    changes for accessories - in parts

--- enabler/src/main/java/com/openxc/enabler/BluetoothReceiver.java
+++ enabler/src/main/java/com/openxc/enabler/BluetoothReceiver.java
@@ -8,10 +8,14 @@ import android.util.Log;
 
 import com.openxc.VehicleManager;
 import com.openxc.enabler.preferences.PreferenceManagerService;
+import com.openxc.interfaces.bluetooth.BluetoothModemVehicleInterface;
+import com.openxc.interfaces.bluetooth.BluetoothV2XVehicleInterface;
 import com.openxc.interfaces.bluetooth.BluetoothVehicleInterface;
 
 public class BluetoothReceiver extends BroadcastReceiver {
     private final static String TAG = BluetoothReceiver.class.getSimpleName();
+
+    public String connectedDeviceId =null;
 
     private final boolean isVehicleInterface(BluetoothDevice device) {
         return device != null && device.getName() != null &&
@@ -24,14 +28,46 @@ public class BluetoothReceiver extends BroadcastReceiver {
         BluetoothDevice bluetoothDevice = (BluetoothDevice)
             intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
         if(isVehicleInterface(bluetoothDevice)) {
-            if(intent.getAction().compareTo(
+            connectedDeviceId = bluetoothDevice.getName();
+	    if(intent.getAction().compareTo(
                         BluetoothDevice.ACTION_ACL_CONNECTED) == 0){
-                Log.d(TAG, "A Bluetooth OpenXC VI connected: " + bluetoothDevice.getName());
-                context.startService(new Intent(context, VehicleManager.class));
-                context.startService(new Intent(context,
-                            PreferenceManagerService.class));
-            } else {
-                Log.d(TAG, "A Bluetooth OpenXC VI disconnected: " + bluetoothDevice.getName());
+             if(bluetoothDevice.getName().startsWith(BluetoothModemVehicleInterface.DEVICE_NAME_PREFIX)) {
+	        // It's a modem.
+	        Log.d(TAG, "A Bluetooth OpenXC Modem connected: " + bluetoothDevice.getName());
+	    
+	        RegisterDevice.setDevice(bluetoothDevice.getName());
+	     } 
+	     else if(bluetoothDevice.getName().startsWith(BluetoothV2XVehicleInterface.DEVICE_NAME_PREFIX)){
+                Log.d(TAG, "A Bluetooth OpenXC V2X connected: " + bluetoothDevice.getName());
+						            		
+	        RegisterDevice.setDevice(bluetoothDevice.getName());
+	    }
+	    else
+            {
+	    	Log.d(TAG, "A Bluetooth OpenXC VI connected: " + bluetoothDevice.getName());
+								            		
+	   	RegisterDevice.setDevice(bluetoothDevice.getName());
+            }
+	    
+	    context.startService(new Intent(context, VehicleManager.class));
+	    context.startService(new Intent(context,
+	                   PreferenceManagerService.class));
+	  }
+	  else {
+	     
+             if(bluetoothDevice.getName().startsWith(BluetoothModemVehicleInterface.DEVICE_NAME_PREFIX)) {
+	      	     Log.d(TAG, "A Bluetooth OpenXC Modem disconnected: " + bluetoothDevice.getName());
+		     RegisterDevice.setDevice(bluetoothDevice.getName());
+	    	}
+	     else if(bluetoothDevice.getName().startsWith(BluetoothV2XVehicleInterface.DEVICE_NAME_PREFIX)){
+	   	     Log.d(TAG, "A Bluetooth OpenXC V2X disconnected: " + bluetoothDevice.getName());
+	    	     RegisterDevice.setDevice(bluetoothDevice.getName());
+		}
+	     else
+	       	{
+	 	     Log.d(TAG, "A Bluetooth OpenXC VI disconnected: " + bluetoothDevice.getName());
+		     RegisterDevice.setDevice(bluetoothDevice.getName());
+		}
             }
         }
     }
