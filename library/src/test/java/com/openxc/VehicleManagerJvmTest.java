3b01674483a83784e9bcfbb3ef269c8278ee5059
Alok Kumar <akuma128@ford.com>
2018-03-28 15:42:08 +0530

    Updated test cases

--- library/src/test/java/com/openxc/VehicleManagerJvmTest.java
+++ library/src/test/java/com/openxc/VehicleManagerJvmTest.java
@@ -1,12 +1,11 @@
 package com.openxc;
 
+import com.openxc.messages.VehicleMessage;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
-import org.robolectric.annotation.Config;
-
-import com.openxc.messages.VehicleMessage;
 
 @RunWith(RobolectricTestRunner.class)
 public class VehicleManagerJvmTest {
@@ -15,7 +14,6 @@ public class VehicleManagerJvmTest {
     @Before
     public void setup()  {
         manager = new VehicleManager();
-        manager.onCreate();
     }
 
     @Test
