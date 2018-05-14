c6e04c3c4c9334793a535f6fc3789b7165fd260f
Alok Kumar <akuma128@ford.com>
2018-05-14 18:04:49 +0530

    Added code to prompt user to enable Bluetooth adapter if it's off #112 Protobuf message parsing for BLE

--- enabler/src/main/java/com/openxc/enabler/StatusFragment.java
+++ enabler/src/main/java/com/openxc/enabler/StatusFragment.java
@@ -1,9 +1,6 @@
 package com.openxc.enabler;
 
-import java.util.Timer;
-import java.util.TimerTask;
-
-import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
@@ -13,7 +10,6 @@ import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.preference.PreferenceManager;
-import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
@@ -31,10 +27,17 @@ import com.openxc.interfaces.bluetooth.DeviceManager;
 import com.openxc.remote.VehicleServiceException;
 import com.openxc.remote.ViConnectionListener;
 import com.openxcplatform.enabler.R;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
 import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+import static android.app.Activity.RESULT_CANCELED;
+import static android.app.Activity.RESULT_OK;
 
 public class StatusFragment extends Fragment {
-    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
+    public static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
+    private static final int REQUEST_ENABLE_BT = 2;
     private static String TAG = "StatusFragment";
 
     private TextView mMessageCountView;
@@ -55,7 +58,7 @@ public class StatusFragment extends Fragment {
     private Context mContext;
 
     private synchronized void updateViInfo() {
-        if(mVehicleManager != null) {
+        if (mVehicleManager != null) {
             // Must run in another thread or we get circular references to
             // VehicleService -> StatusFragment -> VehicleService and the
             // callback will just fail silently and be removed forever.
@@ -72,7 +75,7 @@ public class StatusFragment extends Fragment {
                                 mViPlatformView.setText(platform);
                             }
                         });
-                    } catch(NullPointerException e) {
+                    } catch (NullPointerException e) {
                         // A bit of a hack, should probably use a lock - but
                         // this can happen if this view is being paused and it's
                         // not really a problem.
@@ -89,7 +92,7 @@ public class StatusFragment extends Fragment {
         }
 
         public void onDisconnected() {
-            if(getActivity() != null) {
+            if (getActivity() != null) {
                 getActivity().runOnUiThread(new Runnable() {
                     public void run() {
                         Log.d(TAG, "VI disconnected");
@@ -104,23 +107,23 @@ public class StatusFragment extends Fragment {
 
     private ServiceConnection mConnection = new ServiceConnection() {
         public void onServiceConnected(ComponentName className,
-                IBinder service) {
+                                       IBinder service) {
             Log.i(TAG, "Bound to VehicleManager");
-            mVehicleManager = ((VehicleManager.VehicleBinder)service
-                    ).getService();
+            mVehicleManager = ((VehicleManager.VehicleBinder) service
+            ).getService();
 
             try {
                 mVehicleManager.addOnVehicleInterfaceConnectedListener(
                         mConnectionListener);
-            } catch(VehicleServiceException e) {
+            } catch (VehicleServiceException e) {
                 Log.e(TAG, "Unable to register VI connection listener", e);
             }
 
-            if(getActivity() == null) {
+            if (getActivity() == null) {
                 Log.w(TAG, "Status fragment detached from activity");
             }
 
-            if(mVehicleManager.isViConnected()) {
+            if (mVehicleManager.isViConnected()) {
                 updateViInfo();
             }
 
@@ -129,9 +132,9 @@ public class StatusFragment extends Fragment {
                     try {
                         // It's possible that between starting the thread and
                         // this running, the manager has gone away.
-                        if(mVehicleManager != null) {
+                        if (mVehicleManager != null) {
                             mVehicleManager.waitUntilBound();
-                            if(getActivity() != null) {
+                            if (getActivity() != null) {
                                 getActivity().runOnUiThread(new Runnable() {
                                     public void run() {
                                         mServiceNotRunningWarningView.setVisibility(View.GONE);
@@ -139,7 +142,7 @@ public class StatusFragment extends Fragment {
                                 });
                             }
                         }
-                    } catch(VehicleServiceException e) {
+                    } catch (VehicleServiceException e) {
                         Log.w(TAG, "Unable to connect to VehicleService");
                     }
 
@@ -160,7 +163,7 @@ public class StatusFragment extends Fragment {
         public synchronized void onServiceDisconnected(ComponentName className) {
             Log.w(TAG, "VehicleService disconnected unexpectedly");
             mVehicleManager = null;
-            if(getActivity() != null) {
+            if (getActivity() != null) {
                 getActivity().runOnUiThread(new Runnable() {
                     public void run() {
                         mServiceNotRunningWarningView.setVisibility(View.VISIBLE);
@@ -173,7 +176,7 @@ public class StatusFragment extends Fragment {
     @Override
     public void onResume() {
         super.onResume();
-        if(getActivity() != null) {
+        if (getActivity() != null) {
             getActivity().bindService(
                     new Intent(getActivity(), VehicleManager.class),
                     mConnection, Context.BIND_AUTO_CREATE);
@@ -183,7 +186,7 @@ public class StatusFragment extends Fragment {
     @Override
     public synchronized void onPause() {
         super.onPause();
-        if(mVehicleManager != null) {
+        if (mVehicleManager != null) {
             getActivity().unbindService(mConnection);
             mVehicleManager = null;
         }
@@ -191,7 +194,7 @@ public class StatusFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState) {
+                             Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.status_fragment, container, false);
 
         mServiceNotRunningWarningView = v.findViewById(R.id.service_not_running_bar);
@@ -209,34 +212,7 @@ public class StatusFragment extends Fragment {
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
-
-                        if (ContextCompat.checkSelfPermission(mContext.getApplicationContext(),ACCESS_FINE_LOCATION )
-                                == PackageManager.PERMISSION_GRANTED) {
-                            try {
-                                DeviceManager deviceManager = new DeviceManager(getActivity());
-                                deviceManager.startDiscovery();
-                                // Re-adding the interface with a null address triggers
-                                // automatic mode 1 time
-                                mVehicleManager.setVehicleInterface(
-                                        BluetoothVehicleInterface.class, null);
-
-                                // clears the existing explicitly set Bluetooth device.
-                                SharedPreferences.Editor editor =
-                                        PreferenceManager.getDefaultSharedPreferences(
-                                                getActivity()).edit();
-                                editor.putString(getString(R.string.bluetooth_mac_key),
-                                        getString(R.string.bluetooth_mac_automatic_option));
-                                editor.commit();
-                            } catch (BluetoothException e) {
-                                Toast.makeText(getActivity(),
-                                        "Bluetooth is disabled, can't search for devices",
-                                        Toast.LENGTH_LONG).show();
-                            } catch (VehicleServiceException e) {
-                                Log.e(TAG, "Unable to enable Bluetooth vehicle interface", e);
-                            }
-                        } else {
-                            ActivityCompat.requestPermissions(getActivity(), new String[]{ACCESS_FINE_LOCATION}, LOCATION_PERMISSION_REQUEST_CODE);
-                        }
+                        startBluetoothSearch();
                     }
                 });
 
@@ -249,9 +225,60 @@ public class StatusFragment extends Fragment {
         return v;
     }
 
+    private void startBluetoothSearch() {
+        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        if (isLocationPermissionGranted() && mBluetoothAdapter.isEnabled()) {
+            try {
+                DeviceManager deviceManager = new DeviceManager(getActivity());
+                deviceManager.startDiscovery();
+                // Re-adding the interface with a null address triggers
+                // automatic mode 1 time
+                mVehicleManager.setVehicleInterface(
+                        BluetoothVehicleInterface.class, null);
+
+                // clears the existing explicitly set Bluetooth device.
+                SharedPreferences.Editor editor =
+                        PreferenceManager.getDefaultSharedPreferences(
+                                getActivity()).edit();
+                editor.putString(getString(R.string.bluetooth_mac_key),
+                        getString(R.string.bluetooth_mac_automatic_option));
+                editor.commit();
+            } catch (BluetoothException e) {
+                Toast.makeText(getActivity(),
+                        "Bluetooth is disabled, can't search for devices",
+                        Toast.LENGTH_LONG).show();
+            } catch (VehicleServiceException e) {
+                Log.e(TAG, "Unable to enable Bluetooth vehicle interface", e);
+            }
+        } else if (!mBluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        } else if (!isLocationPermissionGranted()) {
+            requestPermissions(new String[]{ACCESS_FINE_LOCATION}, LOCATION_PERMISSION_REQUEST_CODE);
+        }
+    }
+
+    private boolean isLocationPermissionGranted() {
+        return ContextCompat.checkSelfPermission(mContext.getApplicationContext(), ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED;
+    }
+
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
         this.mContext = context;
     }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_CANCELED) {
+            Toast.makeText(getActivity(), "Bluetooth needs to be enabled for search.",
+                    Toast.LENGTH_LONG).show();
+        } else if (resultCode == RESULT_OK) {
+            startBluetoothSearch();
+        }
+    }
+
 }
