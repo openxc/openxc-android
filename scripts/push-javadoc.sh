c3ea982d48f6cc52a1cb98c02d31c0a77da06029
Prateek Prakash <59665714+PPRAKA30@users.noreply.github.com>
2020-02-19 14:30:55 -0500

    Add script to automate pushing JavaDoc

---
+++ scripts/push-javadoc.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ "$TRAVIS_REPO_SLUG" == "openxc/openxc-android" ] && [ "$TRAVIS_JDK_VERSION" == "openjdk8" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "automate-javadoc" ]; then
+
+  cp -R library/build/docs/javadoc $HOME/javadoc-latest
+
+  cd $HOME
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis-CI"
+  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/openxc/openxc-android gh-pages > /dev/null
+
+  cd gh-pages
+  git rm -rf ./
+  cp -Rf $HOME/javadoc-latest/. ./
+  git add -f .
+  $LATEST_TAG=git describe master --abbrev=0 --tags
+  git commit -m "JavaDoc $LATEST_TAG - Travis Build $TRAVIS_BUILD_NUMBER"
+  git push -fq origin gh-pages > /dev/null
+  
+fi
