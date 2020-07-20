0cc5a9bc2e7f09bdd809d9ba7902deed33fa8024
Stocke <MSTOCKE5@ford.com>
2020-07-20 17:40:12 -0400

    successfully enter text in DiagnosticRequest screen

--- enabler/src/androidTest/java/com/openxc/ui/DiagnosticUITests.java
+++ enabler/src/androidTest/java/com/openxc/ui/DiagnosticUITests.java
@@ -12,8 +12,10 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import androidx.test.espresso.action.ViewActions;
+import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
-import androidx.test.runner.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
@@ -23,25 +25,29 @@ import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 @LargeTest
-//@RunWith(AndroidJUnit4ClassRunner.class)
-@RunWith(AndroidJUnit4.class)
+@RunWith(AndroidJUnit4ClassRunner.class)
 public class DiagnosticUITests {
 
     //*
     private String stringToBetyped;
 
     @Rule
-    public FragmentTestRule<?, DiagnosticRequestFragment> fragmentTestRule =
-            FragmentTestRule.create(DiagnosticRequestFragment.class);
+    public ActivityTestRule<OpenXcEnablerActivity> mActivityTestRule = new ActivityTestRule<>(OpenXcEnablerActivity.class);
 
     @Before
     public void initValidString() {
         // Specify a valid string.
-        stringToBetyped = "Espresso";
+        stringToBetyped = "BB8";
     }
 
     @Test
     public void changeText_sameActivity() {
+
+        //DiagnosticRequestFragment
+        onView(ViewMatchers.withId(R.id.pager_title_strip)).perform(ViewActions.swipeLeft());
+        onView(ViewMatchers.withId(R.id.pager_title_strip)).perform(ViewActions.swipeLeft());
+        onView(ViewMatchers.withId(R.id.pager_title_strip)).perform(ViewActions.swipeLeft());
+
         // Type text
         onView(withId(R.id.diag_request_id)).perform(typeText(stringToBetyped), closeSoftKeyboard());
 
