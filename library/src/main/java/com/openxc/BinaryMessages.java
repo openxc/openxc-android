6d41f8dea2357c00a9b3fbfcd5332ae06853b139
vinodsama <vsama@ford.com>
2020-08-11 11:08:54 -0400

    Removed code for MessagePack

--- library/src/main/java/com/openxc/BinaryMessages.java
+++ library/src/main/java/com/openxc/BinaryMessages.java
@@ -6448,10 +6448,6 @@ public final class BinaryMessages {
        * <code>PROTOBUF = 2;</code>
        */
       PROTOBUF(2),
-      /**
-       * <code>MESSAGEPACK = 3;</code>
-       */
-      MESSAGEPACK(3),
       UNRECOGNIZED(-1),
       ;
 
@@ -6467,10 +6463,6 @@ public final class BinaryMessages {
        * <code>PROTOBUF = 2;</code>
        */
       public static final int PROTOBUF_VALUE = 2;
-      /**
-       * <code>MESSAGEPACK = 3;</code>
-       */
-      public static final int MESSAGEPACK_VALUE = 3;
 
 
       public final int getNumber() {
@@ -6494,7 +6486,6 @@ public final class BinaryMessages {
           case 0: return UNUSED;
           case 1: return JSON;
           case 2: return PROTOBUF;
-          case 3: return MESSAGEPACK;
           default: return null;
         }
       }
