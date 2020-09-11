786ecf485cfa2be2a356dc3ff02b9cf4aa291541
vinodsama <vsama@ford.com>
2020-09-11 11:20:27 -0400

    Testcase added

--- enabler/src/androidTest/java/com/openxc/ui/StatusFragmentUITests.java
+++ enabler/src/androidTest/java/com/openxc/ui/StatusFragmentUITests.java
@@ -45,17 +45,15 @@ public class StatusFragmentUITests {
         onView(withText("Bluetooth needs to be enabled for search.")).check(doesNotExist());
         onView(withText("No Trace File")).check(doesNotExist());
     }
-/*
+
     @Test
     public void start_bluetooth_search_btn_click(){
-       onView(withId(R.id.start_bluetooth_search_btn)).perform(click());
+        onView(withText("SEARCH FOR BLUETOOTH VI")).perform(click());
     }
 
     @Test
     public void disconnect_btn_click() {
-        onView(withId(R.id.disconnect_btn)).perform(click());
+        onView(withText("DISCONNECT")).perform(click());
     }
-
- */
 
 }
