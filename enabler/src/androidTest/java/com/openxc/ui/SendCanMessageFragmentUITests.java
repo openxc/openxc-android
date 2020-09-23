a3b02ea2f461519d09fe4e544168facabb83c8c9
vinodsama <vsama@ford.com>
2020-09-23 11:30:07 -0400

    change inflate class

--- enabler/src/androidTest/java/com/openxc/ui/SendCanMessageFragmentUITests.java
+++ enabler/src/androidTest/java/com/openxc/ui/SendCanMessageFragmentUITests.java
@@ -31,8 +31,8 @@ public class SendCanMessageFragmentUITests {
 
     @Before
     public void setup(){
-    v=  (LinearLayout)  LayoutInflater.from(mActivityTestRule.getActivity()).inflate(R.layout.send_can_message_fragment,null);
-
+    //v=  (LinearLayout)  LayoutInflater.from(mActivityTestRule.getActivity()).inflate(R.layout.send_can_message_fragment,null);
+        v = View.inflate(mActivityTestRule.getActivity().getApplicationContext(), R.layout.send_can_message_fragment, null);
    }
 
     @Test
