a36f55a0fe9cd9cfa730dafdd810816aa436fb1e
vinodsama <vsama@ford.com>
2020-10-05 13:22:12 -0400

    changes to testcase

--- enabler/src/androidTest/java/com/openxc/ui/SendCanMessageFragmentUITests.java
+++ enabler/src/androidTest/java/com/openxc/ui/SendCanMessageFragmentUITests.java
@@ -5,6 +5,7 @@ import android.test.suitebuilder.annotation.LargeTest;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 
 import com.android21buttons.fragmenttestrule.FragmentTestRule;
 import com.openxc.enabler.OpenXcEnablerActivity;
@@ -27,27 +28,35 @@ public class SendCanMessageFragmentUITests {
 
     @Rule
     public FragmentTestRule<?, Fragment> mActivityTestRule = new FragmentTestRule<>(OpenXcEnablerActivity.class,Fragment.class);
-    View v;
+    private View v;
 
-    @Before
-    public void setup(){
-        try{
-           // v = View.inflate(mActivityTestRule.getActivity().getApplicationContext(), R.layout.send_can_message_fragment,null);
-            v= LayoutInflater.from(mActivityTestRule.getActivity()).inflate(R.layout.send_can_message_fragment,null);
-        }
-        catch(Exception e){
-            Log.e("Inflate Exception",Log.getStackTraceString(e));
-        }
 
-   }
+//    @Before
+//    public void setup(){
+//        try{
+//           // v = View.inflate(mActivityTestRule.getActivity().getApplicationContext(), R.layout.send_can_message_fragment,null);
+//           // v= LayoutInflater.from(mActivityTestRule.getActivity()).inflate(R.layout.send_can_message_fragment,null);
+//        }
+//        catch(Exception e){
+//            Log.e("Inflate Exception",Log.getStackTraceString(e));
+//        }
+//
+//   }
+    @SuppressWarnings("TypeParameterUnusedInFormals")
+    private <T extends View> T inflate(int layoutResId) {
+        return (T) LayoutInflater.from(mActivityTestRule.getActivity().getApplicationContext()).inflate(layoutResId, null);
+    }
 
     @Test
     public void check_for_view_layout_data(){
         Log.i("Android version", Build.VERSION.RELEASE);
+        v=inflate( R.layout.send_can_message_fragment);
         assertNotNull(v);
     }
+
     @Test
     public void check_elements_presence() {
+        v=inflate( R.layout.send_can_message_fragment);
         assertNotNull(v.findViewById(R.id.message_id));
         assertNotNull(v.findViewById(R.id.message_payload));
         assertNotNull(v.findViewById(R.id.message_payload2));
